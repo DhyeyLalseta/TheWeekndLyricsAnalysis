7f46c58e9be4245a180698012cb300361981f2ba
dhyey <dhyeylalseta@outlook.com>
2020-11-13 17:48:32 -0700

    genius api integration

---
+++ data_collection/genius_lyrics.py
@@ -0,0 +1,18 @@
+from secrets import *
+from lyricsgenius import Genius
+
+
+genius = Genius(client_access_token)
+
+
+def get_artist_lyrics(artist_name: str):
+    if not artist_name:
+        raise ValueError("No artist name")
+    genius.remove_section_headers = True
+    genius.excluded_terms = ["Remix", "Live"]
+    artist = genius.search_artist(artist_name)
+    artist.save_lyrics("_".join((artist_name.lower()).split()) + ".json")
+
+
+if __name__ == "__main__":
+    get_artist_lyrics("The Weeknd")
