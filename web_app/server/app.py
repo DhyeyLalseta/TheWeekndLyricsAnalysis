25864a6405615b0a6c26d6e8ad28f93073aca158
dhyey <dhyeylalseta@outlook.com>
2020-11-21 01:29:38 -0700

    configuring flask for build

--- web_app/server/app.py
+++ web_app/server/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, request
 from ml.model import TextGenerationModel
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="build", static_url_path="/")
 trilogy_KS_model = TextGenerationModel(
     "./ml/trilogy_model/", "./ml/trilogy_tokenizer.pickle"
 )
 post_KS_model = TextGenerationModel("./ml/modern_model", "./ml/modern_tokenizer.pickle")
+
+
+@app.route("/")
+def index():
+    return app.send_static_file("index.html")
 
 
 @app.route("/api/generate", methods=["POST"])
@@ -17,12 +22,15 @@ def generate_text():
         temperature = request_body["temperature"]
         word_count = request_body["wordCount"]
     except TypeError as e:
-        return {"error": str(e)}
+        return str(e), 400
     response["inputText"] = seed_text
-    response["trilogyGeneratedText"] = trilogy_KS_model.generate_text(
-        seed_text, word_count, temperature
-    )
-    response["modernGeneratedText"] = post_KS_model.generate_text(
-        seed_text, word_count, temperature
-    )
+    try:
+        response["trilogyGeneratedText"] = trilogy_KS_model.generate_text(
+            seed_text, word_count, temperature
+        )
+        response["modernGeneratedText"] = post_KS_model.generate_text(
+            seed_text, word_count, temperature
+        )
+    except Exception as e:
+        return str(e), 500
     return response
