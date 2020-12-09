6759f40f4937e3c46677b34363a7868e01e6d6f2
DhyeyLalseta <dhyeylalseta@outlook.com>
2020-12-09 01:47:45 -0700

    Update genius_lyrics.py

--- data_collection/genius_lyrics.py
+++ data_collection/genius_lyrics.py
@@ -1,9 +1,7 @@
 from secrets import *
 from lyricsgenius import Genius
 
-
 genius = Genius(client_access_token)
-
 
 def get_artist_lyrics(artist_name: str):
     if not artist_name:
@@ -12,7 +10,6 @@ def get_artist_lyrics(artist_name: str):
     genius.excluded_terms = ["Remix", "Live"]
     artist = genius.search_artist(artist_name)
     artist.save_lyrics("_".join((artist_name.lower()).split()) + ".json")
-
 
 if __name__ == "__main__":
     get_artist_lyrics("The Weeknd")
