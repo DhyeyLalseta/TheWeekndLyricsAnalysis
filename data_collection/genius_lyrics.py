4d1dee900b5069db75c76be78ccb0afb99b1a2a5
DhyeyLalseta <dhyeylalseta@outlook.com>
2020-12-09 14:02:37 -0700

    Update genius_lyrics.py

--- data_collection/genius_lyrics.py
+++ data_collection/genius_lyrics.py
@@ -1,11 +1,9 @@
-from secrets import *
+from secrets import client_access_token
 from lyricsgenius import Genius
 
 genius = Genius(client_access_token)
 
 def get_artist_lyrics(artist_name: str):
-    if not artist_name:
-        raise ValueError("No artist name")
     genius.remove_section_headers = True
     genius.excluded_terms = ["Remix", "Live"]
     artist = genius.search_artist(artist_name)
